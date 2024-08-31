acbc7ac334ea7bb16682abbefd32beb2b8302d89
Daniel Ramos <daniel.r.ramos@icloud.com>
2024-08-31 13:38:34 -0400

    Bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,23 +1,28 @@
-//! This crate provides Go language support for the [tree-sitter][] parsing library.
+// -*- coding: utf-8 -*-
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2020, tree-sitter-go authors.
+// See the LICENSE file in this repo for license details.
+// ------------------------------------------------------------------------------------------------
+
+//! This crate provides a Go grammar for the [tree-sitter][] parsing library.
 //!
-//! Typically, you will use the [language][language func] function to add this language to a
+//! Typically, you will use the [language][language func] function to add this grammar to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
 //! use tree_sitter::Parser;
 //!
 //! let code = r#"
-//! func add(a, b int) int {
-//!     return a + b
-//! }
+//!     func double(x int) int {
+//!     	return x * 2
+//!     }
 //! "#;
 //! let mut parser = Parser::new();
-//! let language = tree_sitter_go::LANGUAGE;
-//! parser
-//!     .set_language(&language.into())
-//!     .expect("Error loading Go parser");
-//! let tree = parser.parse(code, None).unwrap();
-//! assert!(!tree.root_node().has_error());
+//! parser.set_language(tree_sitter_go::language()).expect("Error loading Go grammar");
+//! let parsed = parser.parse(code, None);
+//! # let parsed = parsed.unwrap();
+//! # let root = parsed.root_node();
+//! # assert!(!root.has_error());
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
@@ -25,33 +30,40 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter_language::LanguageFn;
+use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_go() -> *const ();
+    fn tree_sitter_go() -> Language;
 }
 
-/// The tree-sitter [`LanguageFn`] for this grammar.
-pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_go) };
+/// Returns the tree-sitter [Language][] for this grammar.
+///
+/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
+pub fn language() -> Language {
+    unsafe { tree_sitter_go() }
+}
+
+/// The source of the Go tree-sitter grammar description.
+pub const GRAMMAR: &'static str = include_str!("../../grammar.js");
+
+/// The syntax highlighting query for this language.
+pub const HIGHLIGHT_QUERY: &'static str = include_str!("../../queries/highlights.scm");
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
-
-/// The syntax highlighting query for this language.
-pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
 
 /// The symbol tagging query for this language.
-pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
+pub const TAGGING_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_can_load_grammar() {
+    fn can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::LANGUAGE.into())
-            .expect("Error loading Go parser");
+            .set_language(super::language())
+            .expect("Error loading Go grammar");
     }
 }
